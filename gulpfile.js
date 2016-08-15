cf36f29647c7086caf84f0aabd50e7d3873da9ba
Geisson <geisonnm@hotmail.com>
2016-08-14 21:56:08 -0300

    gulp tasks add for sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+
+gulp.task('default', ['sass', "sass:watch"]);
+
+
+gulp.task('sass', function () {
+  return gulp.src('./static/css/*.scss')
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(gulp.dest('./static/css/final'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./static/css/*.scss', ['sass']);
 });
